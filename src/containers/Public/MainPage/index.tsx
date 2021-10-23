f4884d04af6bbfb71a64f74216d878fb0005457d
Serhiy Korytskyy <korysergey55@gmail.com>
2021-10-23 18:13:19 +0300

    distinationMap

--- src/containers/Public/MainPage/index.tsx
+++ src/containers/Public/MainPage/index.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react'
 import styles from './styles.module.scss'
 import Header from 'components/Header'
+import MapPage from '../MapPage'
 
 const MainPage = () => {
   return (
     <>
       <Header />
+      <MapPage />
     </>
   )
 }
